b462d86845e507f7c7fe8a31c4db7714cc450fdb
Volt47 <81244228+Volt47@users.noreply.github.com>
2026-05-31 10:31:16 +0200

    Main Menu Logic Implemented
    The player can now go from main menu to reading the letter and then straight into the main playing scene.

--- project-space/Assets/Scripts/MainMenuSCript.cs
+++ project-space/Assets/Scripts/MainMenuSCript.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 
 
 public class MainMenuSCript : MonoBehaviour
 {
-public int scene;
+public int scene = 0;
+public Image letter;
+public Button letterCloser;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        letter.enabled = false;
+        letterCloser.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,9 +24,20 @@ public int scene;
         
     }
 
+    public void revealLetter() 
+    {
+        letter.enabled = true;
+        letterCloser.gameObject.SetActive(true);
+    }
+
+    public void closeLetter()
+    {
+        SceneManager.LoadScene(scene + 1);
+    }
+
     public void ChangeScene()
     {
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadScene(scene + 1);
 
     }
 }
